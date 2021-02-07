e7afbb38841c0340884a159b2a18498e3a459ab8
Akshit Sadana <akshitsadana@gmail.com>
2021-02-07 19:28:44 +0530

    mapping function playground added

--- playground/playground.go
+++ playground/playground.go
@@ -1,1 +1,26 @@
-package playground
+package main
+
+import "fmt"
+
+func f(p string) {
+	fmt.Println("function f parameter:", p)
+}
+
+func g(p string, q int) {
+	fmt.Println("function g parameters:", p, q)
+}
+
+func main() {
+	m := map[string]interface{}{
+		"f": f,
+		"g": g,
+	}
+
+	m["f"].(func(string))("asd")
+
+	s := map[string]func(string){
+		"f": f,
+	}
+
+	s["f"]("asd2")
+}
