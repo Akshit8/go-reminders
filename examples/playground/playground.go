fe1320585f579182a3aae3cb8831a18f4f7702e4
Akshit Sadana <akshitsadana@gmail.com>
2021-02-07 20:35:55 +0530

    function chaining

--- examples/playground/playground.go
+++ examples/playground/playground.go
@@ -1,6 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+)
+
+func d() func(string) error {
+	return func(p string) error {
+		fmt.Println("function d param: ", p)
+		return nil
+	}
+}
+
+func e(p string) {
+	fmt.Println("function e parameter:", p)
+}
 
 func f(p string) {
 	fmt.Println("function f parameter:", p)
@@ -19,8 +33,23 @@ func main() {
 	m["f"].(func(string))("asd")
 
 	s := map[string]func(string){
+		"e": e,
 		"f": f,
 	}
 
-	s["f"]("asd2")
+	cmd, ok := s["e"]
+	if !ok {
+		log.Fatal("key now found")
+	}
+	cmd("Asd")
+
+	r := map[string]func() func(string) error{
+		"d": d,
+	}
+
+	cmd2, ok := r["d"]
+	if !ok {
+		log.Fatal("key now found")
+	}
+	cmd2()("asdfasdasdad")
 }
